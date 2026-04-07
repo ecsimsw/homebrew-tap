c1af68ca7ad446d40f480f3be4d20f0f703f3913
JinHwanKim <ecsimsw@gmail.com>
2026-04-07 12:26:57 +0900

    Update mdviewer.rb

--- Casks/mdviewer.rb
+++ Casks/mdviewer.rb
@@ -1,20 +1,20 @@
-cask "mdviewer" do                                                                                                                                                                                                                                     
-  version "1.0.2"                                                                                                                                                                                                                                    
-  sha256 "02e2d25315749d7d45fd2e1be7eb48f4e049c08649b3d52157d1184158c776d7"
-                                                                                                                                                                                                                                                       
-  url "https://github.com/ecsimsw/mdviewer/releases/download/v#{version}/MarkdownViewer.app.zip"                                                                                                                                                       
-  name "MarkdownViewer"                                                                                                                                                                                                                                
-  desc "Markdown file viewer for macOS"                                                                                                                                                                                                                
-  homepage "https://github.com/ecsimsw/mdviewer"                                                                                                                                                                                                     
-                                                                                                                                                                                                                                                       
+cask "mdviewer" do
+  version "1.1.1"
+  sha256 "2552103e0f09d7118087404f606683d079f36af2e26fa0d36dcea4731c6e793b"
+
+  url "https://github.com/ecsimsw/mdviewer/releases/download/v#{version}/MarkdownViewer.app.zip"
+  name "MarkdownViewer"
+  desc "Markdown file viewer for macOS"
+  homepage "https://github.com/ecsimsw/mdviewer"
+
   depends_on formula: "node"
-                                                                                                                                                                                                                                                       
-  app "MarkdownViewer.app"                                                                                                                                                                                                                           
+
+  app "MarkdownViewer.app"
 
   postflight do
     system_command "/bin/bash",
                    args: ["-c", "export PATH=#{HOMEBREW_PREFIX}/bin:$PATH && npm install -g @ecsimsw/mdviewer"]
-  end                                                                                                                                                                                                                                                  
- 
-  zap trash: "~/Applications/MarkdownViewer.app"                                                                                                                                                                                                       
-end      
+  end
+
+  zap trash: "~/Applications/MarkdownViewer.app"
+end
